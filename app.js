1ccf82b757cec2a9d276dbb97e1e111fd61fe2f5
Ubuntu <ubuntu@ip-172-31-18-66.ec2.internal>
2023-11-20 10:18:47 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World with a Positive Attitude!'));
+app.get('/', (req, res) => res.send('Hello World, Never ever Give Up!We have successfully deployed our application'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
